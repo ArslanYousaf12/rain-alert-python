70a30accb371c0cd08e026ef29568b929636a3ac
Arslan Yousaf <devtechofficial90@gmail.com>
2025-05-14 11:47:51 +0500

    Refactor weather notification logic to include Twilio message sending

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import requests
+from twilio.rest import Client
+
 
 API_KEY = API_KEY
 LAT = "4.710989"
 LONG = "-74.072090"
 OWP_ENDPOINT = "https://pro.openweathermap.org/data/2.5/forecast/hourly"
-
+account_sid = YOUR_ACCOUNT_ID
+auth_token = YOUR_ACCESS_TOKEN
 para = {
     "lat":LAT,
     "lon":LONG,
@@ -24,7 +27,16 @@ for num in range(12):
         bring_umbrela = True
         print(num)
 if bring_umbrela:
-    print("You have to brings umbrela ")
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+    messaging_service_sid='MG5953858eb335c43e62b56a4b0c53ef0f',
+    body='Its going to Rain today so bring your 🌂',
+    to=YOUR_NUMBER
+    )
+    print(message.status)
+
+
+    
 else:
     print("you dont have to brings umbrela")
 
